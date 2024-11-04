88737b7fcf8aeffc77c8efee02598e605a7f5579
Delsielly <135617801+Delsielly@users.noreply.github.com>
2024-11-04 11:37:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'Berserk',
+    'Quem é o protagonista de Berserk?',
+    'Guts'
+
+)
+
+criaCartao(
+    'Berserk',
+    'Qual é o nome do evento que marca a grande traição em Berserk',
+    'O Eclipse'
+
+)
+
+criaCartao(
+    'Berserk',
+    'Qual é a marca que Guts carrega em seu corpo?',
+    'A marca do Sacrifício'
+)
+
+criaCartao(
+    'Berserk',
+    'Se Guts tivese a chance de dar um soco em qualquer personagem, quem ele escolheria?',
+    'Griffith (ele já fez isso várias vezes).'
+)
